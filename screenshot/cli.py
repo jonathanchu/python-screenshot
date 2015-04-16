f290583291c329c0bbc1830db0279ea5a7f94140
Jonathan Chu <jonathan.chu@me.com>
2015-04-15 23:40:18 -0400

    Pass in a page arg to walk all pages in a url

--- screenshot/cli.py
+++ screenshot/cli.py
@@ -9,10 +9,18 @@ def webkit2png(url):
 
 
 @click.command()
+@click.option('--all-pages', '-p', is_flag=True, help='Look for all pages.')
 @click.argument('url', required=True)
-def main(url):
+def main(url, all_pages):
     """Takes a full page screenshot with webkit2png."""
     click.echo('url is {}.'.format(url))
     response = requests.get(url)
     assert response.status_code == 404
-    webkit2png(url)
+
+    page_num = 1
+
+    if all_pages:
+        page = url + '?page={}'.format(page_num)
+        response = requests.get(page)
+        response.raise_for_status()
+        webkit2png(page)
